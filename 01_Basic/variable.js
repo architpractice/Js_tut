32deffa96f49f3bdd1c0dca15d67fd71737c23ca
architpractice <85490497+architpractice@users.noreply.github.com>
2024-06-22 18:40:19 +0000

    variable

---
+++ 01_Basic/variable.js
@@ -0,0 +1,11 @@
+const accountId = 1445 
+let accountemail="architsaxena08454895657@gmail.com"
+var accountpassword="12345"//initilaly js didnt work the scope variable ...still let and const used
+accountCity="Mumbai"//variable can be created without var keyword dont prefer this
+
+//accountId=2 -Const variable cant be changed
+accountemail="archit@gmail.com"
+accountpassword="19999"
+accountCity="Mum"
+console.log(accountId)
+console.table([accountId,accountemail,accountpassword,accountCity])
