98f7041fea892403e404b2deec2f6d2766bd0172
architpractice <85490497+architpractice@users.noreply.github.com>
2024-06-23 03:11:06 +0000

    Conversion_vid6

---
+++ basic_js/variable2.js
@@ -0,0 +1,26 @@
+//alert(3+3) //we are using nodejs not browser
+//Datatypes 
+//number,bigint,string,boolean,null(it is object type ),undefined(it is undefined),symbol,OBJECT
+
+let names="archit"
+let age=23
+console.log(typeof names)
+console.log(typeof null)
+console.log(typeof undefined)
+
+//Datatype Conversion 
+
+let score ="33abc"
+console.log(typeof score)
+console.log(typeof(score))
+let score_num=Number(score)
+console.log(typeof score_num)
+console.log(score_num)
+
+console.log("For 0")
+let emp =null
+console.log(typeof emp)
+console.log(typeof(emp))
+let emp_null=Number(emp)
+console.log(typeof emp_null)
+
