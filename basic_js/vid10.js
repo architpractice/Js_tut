4c6c07b57101384dfb8c4166076060707d47d314
architpractice <85490497+architpractice@users.noreply.github.com>
2024-06-23 07:30:13 +0000

    objfun

---
+++ basic_js/vid10.js
@@ -0,0 +1,15 @@
+const id=Symbol('123')
+const anotherid=Symbol('123')
+console.log(id===anotherid)
+//Array 
+const city=["Ironman","spiderman","captain America"]
+//object 
+let myobj={
+    name:"archit",
+    age: 24,
+    city:"Mumbai"
+}
+//Function
+const fun=function(){
+    console.log('hello');
+}
