853672a3831d3bdd3a8f6e8d00a1592bfbe9f1f4
architpractice <85490497+architpractice@users.noreply.github.com>
2024-06-23 08:10:31 +0000

    string functions

---
+++ basic_js/vid11.js
@@ -0,0 +1,11 @@
+const name="Archit"
+const rep=50
+console.log(name[0])
+console.log(name.length)
+console.log(name.toUpperCase())
+const newsubstring=name.substring(0,3)
+console.log(newsubstring)
+const newslices=name.slice(-8,2)
+console.log(newslices)
+const newtrimstring="  Archit  "
+console.log(newtrimstring.trim())
