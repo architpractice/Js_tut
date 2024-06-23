dec72b4c75fb70bb98f926d7dba5ff63f09e8a5d
architpractice <85490497+architpractice@users.noreply.github.com>
2024-06-23 12:56:24 +0000

    Dates,Maths,conversion

---
+++ basic_js/vid12.js
@@ -0,0 +1,18 @@
+//Externslly defining
+const balance = new Number(100)
+console.log(balance)
+console.log(balance.toString().length)
+//Maths
+console.log(Math.abs(-1))
+console.log(Math.round(1.6))
+console.log(Math.max(4,3,2,1))
+console.log(Math.random())//Give value between 0 and 1
+console.log(Math.floor(Math.random()*10+1))
+ //Dates
+ let myDate= new Date()
+ console.log(myDate)
+ console.log(myDate.toDateString())
+ console.log(myDate.toLocaleString())
+//Declare 
+let createDate=new Date(2024,4,5)
+console.log(createDate.toDateString())//Sun May 05 2024
